024e3cdb8dd9315e2fafe07cb6fb0f315504d3b2
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-06 02:40:24 +0300

    Changes

--- blog_generator/views.py
+++ blog_generator/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
@@ -15,10 +17,17 @@ def user_signup(request):
         repeatPassword = request.POST['repeatPassword']
         
         if password == repeatPassword:
-            pass
+            try:
+                user = User.objects.create_user(username, email, password,)
+                user.save()
+                login(request, user)
+                return redirect('/')
+            except:
+                error_message = 'Error creating account'
+                return render(request, 'signup.html', {'error_message:error_message'})
         else:
             error_message = 'Password do not match'
-            return render(request, 'signup.html' {'error_message:error_message'})
+            return render(request, 'signup.html', {'error_message:error_message'})
     return render(request, 'signup.html')
 
 def user_logout(request):
