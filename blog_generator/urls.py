911d10cf27555c6bfd71ede177c9e6f81b13b966
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-06 01:16:56 +0300

    New changes

--- blog_generator/urls.py
+++ blog_generator/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.user_login, name='login'),
+    path('signup', views.user_signup, name='signup'),
+    path('logout', views.user_logout, name='logout'),
 ]
 
 
--- blog_generator/views.py
+++ blog_generator/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def user_login(request):
+    pass
+
+def user_signup(request):
+    pass
+
+def user_logout(request):
+    pass
