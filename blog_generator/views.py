7729c693b30d7bbbaa0fbe5368defee7c5990275
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-06 01:58:44 +0300

    changes

--- blog_generator/views.py
+++ blog_generator/views.py
@@ -13,6 +13,12 @@ def user_signup(request):
         email = request.POST['email']
         password = request.POST['password']
         repeatPassword = request.POST['repeatPassword']
+        
+        if password == repeatPassword:
+            pass
+        else:
+            error_message = 'Password do not match'
+            return render(request, 'signup.html' {'error_message:error_message'})
     return render(request, 'signup.html')
 
 def user_logout(request):
