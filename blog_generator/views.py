1c43f223b3ad048d28fca5783a0a8b106e6794d2
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-06 01:54:56 +0300

    Changes

--- blog_generator/views.py
+++ blog_generator/views.py
@@ -8,6 +8,11 @@ def user_login(request):
     return render(request, 'login.html')
 
 def user_signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        repeatPassword = request.POST['repeatPassword']
     return render(request, 'signup.html')
 
 def user_logout(request):
