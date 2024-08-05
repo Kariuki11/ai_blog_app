4908066ce3926795d35bcb681ffdaf4513ed50ad
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-06 00:21:00 +0300

    New project

---
+++ ai_blog_app/urls.py
@@ -0,0 +1,23 @@
+"""
+URL configuration for ai_blog_app project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path, include  # Import path and include from django.urls
+from django.contrib import admin  # Import admin module from django.contrib
+
+urlpatterns = [
+    path('admin/', admin.site.urls),  # Use admin.site.urls to include Django admin URLs
+    path('', include('blog_generator.urls')),  # Use include to include URLs from another app
+]
