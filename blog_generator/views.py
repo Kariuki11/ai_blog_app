2572f8b8938f1dfb6e0c9c52a2af08d8a71d6772
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-06 10:02:20 +0300

    New changes

--- blog_generator/views.py
+++ blog_generator/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def index(request):
     return render(request, 'index.html')
 
