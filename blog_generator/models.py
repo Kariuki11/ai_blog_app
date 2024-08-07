36b545bdc0ff23c93ad79764af0b163353b4de97
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-07 21:17:21 +0300

    New changes

--- blog_generator/models.py
+++ blog_generator/models.py
@@ -5,3 +5,6 @@ from django.contrib.auth.models import User
 class BlogPost(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     youtube_title = models.CharField(max_length=300)
+    youtube_link = models.URLField()
+    generated_content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
